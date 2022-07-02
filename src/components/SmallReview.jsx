64ec63907fb50afd9c5507f6a2a86d32f4862bc1
kmurp62rulz <kmurp62rulz@gmail.com>
2022-07-02 16:28:13 +1000

    remove stars for small reviews

--- src/components/SmallReview.jsx
+++ src/components/SmallReview.jsx
@@ -8,13 +8,13 @@ const SmallReview = ({ title, img }) => {
       </figure>
       <div class="card-body flex">
         <p class="font-semibold">{title}</p>
-        <div class="rating">
+        {/* <div class="rating">
           <input type="radio" name="rating-1" class="mask mask-star" />
           <input type="radio" name="rating-1" class="mask mask-star" checked />
           <input type="radio" name="rating-1" class="mask mask-star" />
           <input type="radio" name="rating-1" class="mask mask-star" />
           <input type="radio" name="rating-1" class="mask mask-star" />
-        </div>
+        </div> */}
       </div>
     </div>
   );
