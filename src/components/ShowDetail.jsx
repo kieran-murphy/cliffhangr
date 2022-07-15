af2e48986b509e043db5ae2b10da638a2eb2ef35
kmurp62rulz <kmurp62rulz@gmail.com>
2022-07-15 14:45:39 +1000

    fixed trailer link, and added button for adding review

--- src/components/ShowDetail.jsx
+++ src/components/ShowDetail.jsx
@@ -9,24 +9,28 @@ const ShowDetail = ({}) => {
     <div className="min-h-60">
       <img src={data[title].img} alt={title} />
     </div>
-    <div className="my-4 flex flex-row place-content-around">
+    <div className="mx-6">
+    <div className="my-4 flex flex-row place-content-between">
       <div>
         <h1 className="font-bold text-3xl">{data[title].title}</h1>
         <h1 className="font-light text-lg">{data[title].stars} out of 5 stars</h1>
       </div>
       <div className="flex flex-col text-center">
       <h1 className="font-bold text-2xl">2006</h1>
-      <a href={data[title].trailer}>
+      <a href={`https://www.youtube.com/results?sp=mAEA&search_query=${data[title].title}+trailer`}>
       <button className="btn btn-sm btn-secondary">Trailer</button>
       </a>
       </div>
       
 
     </div>
-    <p className="mx-4 my-8 italic font-light">
+    <p className=" my-8 italic font-light">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
     </p>
-    
+    <div className="flex w-full place-content-center">
+    <button className="btn btn-success w-5/6">Write a Review</button>
+    </div>
+     </div>
     </div>
   );
 };
