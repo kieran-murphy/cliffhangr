fd8073c80ba557fb1c11d67df2fb59f8c4ff5637
kmurp62rulz <kmurp62rulz@gmail.com>
2022-08-23 22:44:12 +1000

    fixed user search error

--- server/index.js
+++ server/index.js
@@ -17,31 +17,42 @@ const ShowsSchema = new mongoose.Schema({
   year: Number,
 });
 
-const Listings = mongoose.model(
+const UsersSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    age: Number,
+    following: Array,
+    followers: Array,
+    favorites: Array,
+    profilePicture: String,
+    bio: String,
+  });
+
+const Shows = mongoose.model(
   "shows",
   ShowsSchema,
   "shows"
 );
 
-app.get("/listings/:page/:limit", (req, res) => {
-  const page = parseInt(req.params.page);
-  const limit = parseInt(req.params.limit) + 1;
+const Users = mongoose.model(
+    "users",
+    UsersSchema,
+    "users"
+  );
 
-  //implement pagination
-  Listings.find()
-    .skip(page * limit)
-    .limit(limit)
-    .exec((err, listings) => {
+app.get("/shows/", (req, res) => {
+  Shows.find()
+    .limit(10)
+    .exec((err, shows) => {
       if (err) {
         res.status(500).send(err);
       } else {
-        Listings.estimatedDocumentCount().exec((err, count) => {
+        Shows.estimatedDocumentCount().exec((err, count) => {
           if (err) {
             console.log(err);
           }
-
           res.status(200).send({
-            listings: listings,
+            shows: shows,
             total: count,
           });
         });
@@ -49,6 +60,78 @@ app.get("/listings/:page/:limit", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log("API is running on port 3000");
+app.get("/shows/:id/", (req, res) => {
+    const id = mongoose.ObjectId(req.params.id);
+    console.log(`searching for ${id}`);
+    
+    Shows.find({
+        _id: id,
+      })
+      .exec((err, shows) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          Shows.estimatedDocumentCount({
+            _id: id,
+          }).exec((err, count) => {
+            if (err) {
+              console.log(err);
+            }
+            res.status(200).send({
+              shows: shows,
+              total: count,
+            });
+          });
+        }
+      });
+  });
+
+app.get("/users", (req, res) => {
+    Users.find()
+      .limit(10)
+      .exec((err, users) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          Users.estimatedDocumentCount().exec((err, count) => {
+            if (err) {
+              console.log(err);
+            }
+            res.status(200).send({
+              users: users,
+              total: count,
+            });
+          });
+        }
+      });
+  });
+
+  app.get("/users/:username/", (req, res) => {
+    const username = req.params.username;
+    console.log(`searching for ${username}`);
+    
+    Users.find({
+        name: username,
+      })
+      .exec((err, users) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          Users.estimatedDocumentCount({
+            name: username,
+          }).exec((err, count) => {
+            if (err) {
+              console.log(err);
+            }
+            res.status(200).send({
+              users: users,
+              total: count,
+            });
+          });
+        }
+      });
+  });
+
+app.listen(8080, () => {
+  console.log("API is running on port 8080");
 });
